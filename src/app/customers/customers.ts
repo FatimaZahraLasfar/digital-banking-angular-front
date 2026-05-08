41b5d84bb312943cb783e74f12800a05963a6cad
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-05-08 18:25:51 +0100

    Handle search customers

--- src/app/customers/customers.ts
+++ src/app/customers/customers.ts
@@ -3,24 +3,36 @@ import { catchError, Observable, throwError } from 'rxjs';
 import { CommonModule } from '@angular/common';  // 👈 Import CommonModule
 import { CustomerService } from '../services/customer-service';
 import { Customer } from '../model/customer_model';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-customers',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './customers.html',
   styleUrl: './customers.css',
 })
 export class Customers implements OnInit {
   customers!: Observable<Array<Customer>>;
-  errorMessage! : String;
-  constructor(private customerService: CustomerService) {}
+  errorMessage!: String;
+  searchFormGroup: FormGroup | undefined;
+  constructor(
+    private customerService: CustomerService,
+    private fb: FormBuilder,
+  ) {}
 
   ngOnInit() {
+    this.searchFormGroup = this.fb.group({
+      keyword: this.fb.control(null),
+    });
     this.customers = this.customerService.getCustomers().pipe(
-      catchError(err => {
+      catchError((err) => {
         this.errorMessage = err;
         return throwError(err);
-      })
+      }),
     );
+  }
+
+  handleSearchCustomer() {
+    let kw = this.searchFormGroup?.value.keyword;
   }
 }
