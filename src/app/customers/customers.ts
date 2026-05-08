0215a36fcbd5dd1e625057fb1c9893fa32ffec66
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-05-08 15:03:39 +0100

    Add navbar component

---
+++ src/app/customers/customers.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-customers',
+  imports: [],
+  templateUrl: './customers.html',
+  styleUrl: './customers.css',
+})
+export class Customers implements OnInit {
+  customers: any;
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('http://localhost:8085/customers').subscribe({
+      next: (data) => {
+        this.customers = data;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+}
