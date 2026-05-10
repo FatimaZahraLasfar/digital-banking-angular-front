0ca5d1ebcbaf9996c43f7b0b82b382f8f67b051a
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-05-10 02:59:10 +0100

    inject AuthService, expose username and logout

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../security/auth.service';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './navbar.html',
-  styleUrls: ['./navbar.css'],
+  styleUrl: './navbar.css'
 })
-export class Navbar  implements OnInit {
-  constructor() {}
+export class NavbarComponent implements OnInit {
+  username = '';
 
-  ngOnInit(): void {}
+  constructor(
+    public authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.username = this.authService.getUsername() ?? '';
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
 }
-
