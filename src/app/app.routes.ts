37ae3b9214359ae5fb8938c351f31181b68d1509
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-05-10 02:58:56 +0100

    protect all routes with authGuard, add /login route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,38 @@
 import { Routes } from '@angular/router';
-import { Customers } from './customers/customers';
-import { Accounts } from './accounts/accounts';
-import { NewCustomer } from './new-customer/new-customer';
+import { authGuard } from './security/auth.guard';
 
 export const routes: Routes = [
-  { path: 'customers', component: Customers },
-  { path: 'accounts', component: Accounts },
-  { path: 'new-customer', component: NewCustomer },
-
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login').then(m => m.LoginComponent)
+  },
+  {
+    path: 'customers',
+    loadComponent: () => import('./customers/customers').then(m => m.CustomersComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'accounts',
+    loadComponent: () => import('./accounts/accounts').then(m => m.AccountsComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'new-customer',
+    loadComponent: () => import('./customers/new-customer/new-customer').then(m => m.NewCustomerComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'edit-customer/:id',
+    loadComponent: () => import('./customers/edit-customer/edit-customer').then(m => m.EditCustomerComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: '',
+    redirectTo: 'customers',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'customers'
+  }
 ];
